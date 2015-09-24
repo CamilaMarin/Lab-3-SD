12eb6d2b71841e15d8f67e7e5319c7d6628ab732
YerkoPino <yerko.pino@usach.cl>
2015-09-24 15:53:50 -0300

    Conexiones con 2 bases de datos

---
+++ app.js
@@ -0,0 +1,61 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var mongodb = require('mongodb').MongoClient;
+//Agregado
+var chat = require('./routes/chat_servidor');
+//
+mongodb.connect('mongodb://localhost:3000/primer_base', function(error){
+	if(error){
+		throw error;		
+	}else{
+		console.log('Conectado a MongoDB puerto 3000');
+	}
+});
+mongodb.connect('mongodb://localhost:26999/primer_base', function(error){
+	if(error){
+		throw error;		
+	}else{
+		console.log('Conectado a MongoDB puerto 26999');
+	}
+});
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+//cosas agregadad
+app.get('/chat', function(res, res){
+	res.render('chat');
+});
+//fin
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+//Agregado
+chat.iniciar(server);
