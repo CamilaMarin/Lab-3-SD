6ae787a0434c1065a197449be44f718907d2715c
YerkoPino <yerko.pino@usach.cl>
2015-09-25 13:05:03 -0300

    Insercion en la base de datos

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,50 @@
+var SU = require('../models/alumnos') 
 
-/*
- * GET home page.
- */
+var crypto = require('crypto');
+var key = 'lab3';
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+module.exports = function(app){
+	
+	app.get('/', function(req, res){
+		//SU.list(function(e, subs){
+			res.render('index', { title: 'Lista de suscriptores',error:'' });
+		//})
+		
+	})
+	
+	app.post('/', function(req, res){
+		console.log(req.param('name'));
+		var hash = crypto.createHmac('sha512',key);
+		hash.update(req.param('name'));
+		var value = hash.digest('hex');
+		console.log(value);
+		console.log((parseInt(value,16))%3);
+		SU.new2({name: req.param('name'), email: req.param('email')}, function(o){
+			//SU.list(function(e, subs){
+				res.render('index', { title: 'Lista de suscriptores', error:o});
+			//})
+		})
+	})
+	
+	app.post('/save', function(req,res){
+		SU.edit({name: req.param('name'), email:req.param('email'), id:req.param('id')}, function(o){
+			if(o){
+				res.redirect('/');
+			}else{
+				resp.send('Error al actualiza registro',400)
+			}
+		})
+	})
+	
+	app.post('/delete', function(req, res){
+		SU.delete(req.body.id, function(e,obj){
+			if(!e){
+				res.send('ok',200)
+			}else{
+				res.send('El subscriptor a eliminar no existe', 400)
+			}
+			
+		})
+	})
+	
+}
