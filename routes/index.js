bc1b3125432b5b39ad9bddd350c6c4e7f2b40b3a
YerkoPino <yerko.pino@usach.cl>
2015-09-28 11:24:20 -0300

    Codificacion de rut

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,9 @@ module.exports = function(app){
 	})
 	
 	app.post('/', function(req, res){
-		console.log(req.param('name'));
+		console.log(req.param('rut'));
 		var hash = crypto.createHmac('sha512',key);
-		hash.update(req.param('name'));
+		hash.update(req.param('rut'));
 		var value = hash.digest('hex');
 		console.log(value);
 		console.log((parseInt(value,16))%3);
@@ -51,18 +51,22 @@ module.exports = function(app){
 		var resultado = [];
 		var i;
 		AUX.list({filtro: req.param('filtro')}, function(e, alumnos){
+			console.log('Buscando en base de datos en puerto 3000');
 			for (i = 0; i < alumnos.length; i++) {
 				resultado.push(alumnos[i]);
 			}
 			AUX2.list({filtro: req.param('filtro')}, function(e, alumnos2){
+				console.log('Buscando en base de datos en puerto 26999');
 				for (i = 0; i < alumnos2.length; i++) {
 					resultado.push(alumnos2[i]);
 				}
 				AUX3.list({filtro: req.param('filtro')}, function(e, alumnos3){
+					console.log('Buscando en base de datos en puerto 1515');
 					for (i = 0; i < alumnos3.length; i++) {
 						resultado.push(alumnos3[i]);
 					}
 					AUX4.list({filtro: req.param('filtro')}, function(e, alumnos4){
+						console.log('Buscando en base de datos en puerto 8080');
 						for (i = 0; i < alumnos4.length; i++) {
 							resultado.push(alumnos4[i]);
 						}
