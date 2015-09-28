fbdc69cb0f1056e27f52ee4862c024a1262113f1
YerkoPino <yerko.pino@usach.cl>
2015-09-27 23:44:40 -0300

    Correcciones menores

--- app.js
+++ app.js
@@ -6,14 +6,16 @@
 var express = require('express')
   , http = require('http')
   , path = require('path');
+var favicon = require('serve-favicon');
 
 var app = express();
 
 app.configure(function(){
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.set('port', process.env.PORT || 8989);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  //app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
